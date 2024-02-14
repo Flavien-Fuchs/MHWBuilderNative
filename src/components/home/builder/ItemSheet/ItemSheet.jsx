eee55205dd7aeae6a207fb7392247b48f3890712
kanna 77 <kannavija@gmail.com>
2024-02-14 12:31:35 +0100

    conversion in react native

---
+++ src/components/home/builder/ItemSheet/ItemSheet.jsx
@@ -0,0 +1,123 @@
+import React from "react";
+import { View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { styles } from "../ItemSheet/ItemSheetStyle";
+import PropTypes from "prop-types";
+
+const ItemSheet = ({
+  head,
+  chest,
+  gloves,
+  waist,
+  legs,
+  weapon,
+  charm,
+  setArmorPage,
+  setWeaponPage,
+  setCharmsPage,
+  setBuilder,
+  deleteItem,
+  displayItem,
+  setDisplayItem,
+}) => {
+  const toggleDisplayItem = (type) => {
+    setDisplayItem(type);
+  };
+
+  return (
+    <SafeAreaView>
+      <View style={styles.container}>
+        {displayItem && (
+          <ItemSheetSingle
+            type={displayItem}
+            head={head}
+            chest={chest}
+            gloves={gloves}
+            waist={waist}
+            legs={legs}
+            weapon={weapon}
+            charm={charm}
+            setArmorPage={setArmorPage}
+            setWeaponPage={setWeaponPage}
+            setCharmsPage={setCharmsPage}
+            setBuilder={setBuilder}
+            toggleDisplayItem={toggleDisplayItem}
+            deleteItem={deleteItem}
+          />
+        )}
+        <View style={styles.part}>
+          <ItemHead
+            head={head}
+            toggleDisplayItem={toggleDisplayItem}
+            setArmorPage={setArmorPage}
+            setBuilder={setBuilder}
+          />
+          <ItemCharm
+            charm={charm}
+            toggleDisplayItem={toggleDisplayItem}
+            setCharmsPage={setCharmsPage}
+            setBuilder={setBuilder}
+          />
+        </View>
+
+        <View style={styles.part}>
+          <ItemWeapon
+            weapon={weapon}
+            toggleDisplayItem={toggleDisplayItem}
+            setWeaponPage={setWeaponPage}
+            setBuilder={setBuilder}
+          />
+          <ItemChest
+            chest={chest}
+            toggleDisplayItem={toggleDisplayItem}
+            setArmorPage={setArmorPage}
+            setBuilder={setBuilder}
+          />
+          <ItemGloves
+            gloves={gloves}
+            toggleDisplayItem={toggleDisplayItem}
+            setArmorPage={setArmorPage}
+            setBuilder={setBuilder}
+          />
+        </View>
+
+        <View style={styles.part}>
+          <ItemWaist
+            waist={waist}
+            toggleDisplayItem={toggleDisplayItem}
+            setArmorPage={setArmorPage}
+            setBuilder={setBuilder}
+          />
+        </View>
+
+        <View style={styles.part}>
+          <ItemLegs
+            legs={legs}
+            toggleDisplayItem={toggleDisplayItem}
+            setArmorPage={setArmorPage}
+            setBuilder={setBuilder}
+          />
+        </View>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+ItemSheet.propType = {
+  head: PropTypes.object,
+  chest: PropTypes.object,
+  gloves: PropTypes.object,
+  waist: PropTypes.object,
+  legs: PropTypes.object,
+  weapon: PropTypes.object,
+  charm: PropTypes.object,
+  setArmorPage: PropTypes.func.isRequired,
+  setWeaponPage: PropTypes.func.isRequired,
+  setCharmsPage: PropTypes.func.isRequired,
+  setBuilder: PropTypes.func.isRequired,
+  deleteItem: PropTypes.func.isRequired,
+  displayItem: PropTypes.string,
+  setDisplayItem: PropTypes.func.isRequired,
+};
+
+export default ItemSheet;
--- src/pages/BuilderScreen/BuilderScreen.jsx
+++ src/pages/BuilderScreen/BuilderScreen.jsx
@@ -1,10 +1,10 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
-import { style } from './BuilderScreenStyle'
+import { style } from "./BuilderScreenStyle";
 
 const BuilderScreen = ({ route }) => {
-  const { armors, weapons, charms, skills } = route.params
+  const { armors, weapons, charms, skills } = route.params;
   return (
     <SafeAreaView>
       <View>
@@ -15,4 +15,3 @@ const BuilderScreen = ({ route }) => {
 };
 
 export default BuilderScreen;
-
