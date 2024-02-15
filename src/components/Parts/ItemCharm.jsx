120e4cfc62d20192c940ebc67a05c662a0cfc164
kanna 77 <kannavija@gmail.com>
2024-02-15 10:10:56 +0100

    first

---
+++ src/components/Parts/ItemCharm.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+function ItemCharm({ charm, toggleDisplayItem, setCharmsPage }) {
+  return (
+    <View>
+      <Text>ItemCharm</Text>
+    </View>
+  );
+}
+
+export default ItemCharm;
+
+const styles = StyleSheet.create({});
