ff9e7999ee3226945d7960bae1138a9bfd16236a
AyouBattle <152877288+AyouBattle@users.noreply.github.com>
2024-02-22 17:26:26 +0100

    Amrmos, charm et weapons item en composant

--- src/components/builder/Items/Charm/Charm.jsx
+++ src/components/builder/Items/Charm/Charm.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { View, Text, TextInput, TouchableOpacity, Image, FlatList, ImageBackground, Modal } from 'react-native';
 import { styles } from '../Armors/ArmorsStyle';
+import CharmItem from './CharmItem';
 
 function Charms({ charms, handleCharms, closePage }) {
   const [searchTerm, setSearchTerm] = useState('');
@@ -16,35 +17,6 @@ function Charms({ charms, handleCharms, closePage }) {
       )
     );
   });
-
-  const renderItem = ({ item }) => {
-    const actualCharm = item.ranks[item.ranks.length - 1];
-
-    return (
-      <TouchableOpacity
-        style={styles.item}
-        onPress={() => handleCharms(item)}
-      >
-        <Text style={styles.paragraphList}>{item.name}</Text>
-        <Text style={styles.rarity}>Rarity {actualCharm.rarity}</Text>
-        <Image
-          source={require('../../../../assets/images/amulet3.png')}
-          style={styles.armorImage}
-          resizeMethod="resize"
-        />
-        <View>
-          <Text style={styles.skillsList}>Skills list</Text>
-          <View>
-            {actualCharm.skills.map((skill, skillKey) => (
-              <Text key={skillKey} style={styles.text}>
-                {skill.skillName} - {skill.level}
-              </Text>
-            ))}
-          </View>
-        </View>
-      </TouchableOpacity>
-    );
-  };
 
   return (
     <Modal animationType="slide"
@@ -73,7 +45,9 @@ function Charms({ charms, handleCharms, closePage }) {
       <FlatList
         data={newCharms}
         keyExtractor={(item, index) => index.toString()}
-        renderItem={renderItem}
+        renderItem={({ item }) => (
+          <CharmItem item={item} handleCharms={handleCharms} />
+        )}
       />
     </ImageBackground>
     </Modal>
