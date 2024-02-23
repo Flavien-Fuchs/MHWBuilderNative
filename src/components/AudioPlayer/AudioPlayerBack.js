64096d275c382e1ecbecd29f7da301dd9185de40
samuel-boudine-pro <152878615+samuel-boudine-pro@users.noreply.github.com>
2024-02-23 10:35:50 +0100

    Update AudioPlayerBack.js

--- src/components/AudioPlayer/AudioPlayerBack.js
+++ src/components/AudioPlayer/AudioPlayerBack.js
@@ -2,6 +2,8 @@ import React, { useEffect, useRef } from "react";
 import { View } from "react-native";
 import { Audio } from "expo-av";
 
+const VOLUME = 0.05;
+
 const AudioPlayer = ({ source }) => {
   const sound = useRef(new Audio.Sound());
 
@@ -11,13 +13,15 @@ const AudioPlayer = ({ source }) => {
         await sound.current.loadAsync(
           require("../../assets/audio/MHW_Original_Soundtrack.mp3")
         );
-        if (source === "jeu") {
+        if (source === "Game") {
           await sound.current.loadAsync(
+            // jai pas encore la musique je vais le changer apres pour le jeu
             require("../../assets/audio/MHW_Original_Soundtrack.mp3")
           );
         }
 
         sound.current.setIsLoopingAsync(true);
+        sound.current.setVolumeAsync(VOLUME);
         sound.current.playAsync();
       } catch (error) {
         console.error("Erreur lors du chargement du son", error);
@@ -32,7 +36,7 @@ const AudioPlayer = ({ source }) => {
     };
   }, [source]);
 
-  return <View />; // Vous pouvez rendre n'importe quel composant ici, ou rien du tout
+  return <></>; // Vous pouvez rendre n'importe quel composant ici, ou rien du tout
 };
 
 export default AudioPlayer;
