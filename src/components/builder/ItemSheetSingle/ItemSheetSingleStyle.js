6a2254a4196b88f174018372295e829dbb855e8a
Flavien Fuchs <flavien.fuchs@gmail.com>
2024-02-26 17:10:29 +0100

    save2

--- src/components/builder/ItemSheetSingle/ItemSheetSingleStyle.js
+++ src/components/builder/ItemSheetSingle/ItemSheetSingleStyle.js
@@ -5,7 +5,6 @@ export const styles = StyleSheet.create({
     ItemSheetSingleContainer: {
         flex: 1,
         width: "100%",
-        width: "100%",
         marginTop: 10
     },
     resistance: {
